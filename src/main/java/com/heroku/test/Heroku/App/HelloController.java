4e97b0e261933a01cf3e1475e88f3baeaf33024e
wwasilewski <w.wasilewski@gmail.com>
2019-07-22 10:17:17 +0200

    Initial commit

---
+++ src/main/java/com/heroku/test/Heroku/App/HelloController.java
@@ -0,0 +1,17 @@
+package com.heroku.test.Heroku.App;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Wojciech Wasilewski
+ * @date Created on 22.07.2019
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World in my first Heroku App";
+    }
+}
---
+++ src/main/java/com/heroku/test/Heroku/App/HerokuAppApplication.java
@@ -0,0 +1,13 @@
+package com.heroku.test.Heroku.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuAppApplication.class, args);
+	}
+
+}
